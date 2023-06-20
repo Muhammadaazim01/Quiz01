3b3a634a67d783b81f16f1ecb3cff6c561c24228
Muhammadaazim01 <muhammadaazim96@gmail.com>
2023-06-21 04:06:04 +0500

    addQuiz1file

---
+++ Quiz1.dart
@@ -0,0 +1,9 @@
+void main () {
+  String email = "aazim@gmail.com";
+  String password = "123";
+  if(email == "aazim@gmail.com" && password == "123" ){
+    print ("Right");
+  }else{
+    print("Wrong");
+  }
+}
